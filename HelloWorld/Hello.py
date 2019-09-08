bf7d25bb888c4b6e2b5a46e64c0240f0d6614487
Alex Wang <alex19891118@gmail.com>
2019-09-08 22:39:13 +0800

    20190908 - update more practice final

--- HelloWorld/Hello.py
+++ HelloWorld/Hello.py
@@ -14,3 +14,18 @@ price = 25.6
 exchange_rate = 32.33
 ntd_price = price * exchange_rate
 print(ntd_price)
+
+print("Here is a new line \t and here is the sercond line")
+
+s = 'Hello'
+print(s[0])
+print(s[-1])
+print(s[2:])
+print(s[:1])
+print(s[::2])
+s = s + 'hahahaha'
+print(s[::2])
+
+print(s.upper())
+print(s.split('e'))
+print(dir(s));
