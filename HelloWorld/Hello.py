d24c0db8e0a9b395d2b7da60b50cba9d95c6e692
Alex Wang <alex19891118@gmail.com>
2019-09-08 20:53:57 +0800

    20190908 - update more practice

--- HelloWorld/Hello.py
+++ HelloWorld/Hello.py
@@ -1,10 +1,16 @@
-print ("Hello World!")
-L = [1,2,3,4,5,6]
-print (L)
+print("Hello World!")
+L = [1, 2, 3, 4, 5, 6]
+print(L)
 
 L = {75, 92, 59, 68}
 sum = 0.0
 for a in L:
     sum = sum + a
-print (sum / 4)
-print ("ALEX TEST")
+print(sum / 4)
+print("ALEX TEST")
+
+"""2019/09/08"""
+price = 25.6
+exchange_rate = 32.33
+ntd_price = price * exchange_rate
+print(ntd_price)
